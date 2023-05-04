db5b42447c5db3ea9006264927827d7c22e76ec7
CuongVT <cuongvt@appota.com>
2023-05-04 18:04:34 +0700

    Revoke token

--- backend/middleware/verify-token.js
+++ backend/middleware/verify-token.js
@@ -1,17 +1,25 @@
 import jwt from "jsonwebtoken";
 
-const verifyToken = (req, res, next) => {
+import redisClient from "../redis";
+
+const verifyToken = async (req, res, next) => {
     try {
         const decodedToken = jwt.decode(req.headers.authorization);
 
         const userId = decodedToken.user_id;
-
         if (!userId) {
             return res.status(401).json({ error: "Not logged in" });
         }
 
-        req.user_id = userId;
+        const isBlackList = await redisClient.get(`black-list-token:${userId}`);
+        if (isBlackList) {
+            return res.status(401).json({ error: "Token is invalid" });
+        }
+        if (req.route.path == "/signout") {
+            req.exp_token = decodedToken.exp;
+        }
 
+        req.user_id = userId;
         next();
     } catch (err) {
         return res.status(401).json({ error: "Token is expired or invalid" });
---
+++ backend/redis.js
@@ -0,0 +1,12 @@
+import redis from "redis";
+
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
+
+redisClient.on('connect', () => {
+    console.log('Redis client connected');
+});
+
+export default redisClient;
