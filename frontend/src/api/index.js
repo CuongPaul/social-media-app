2594167d5af5000d4362e024756a5a018d7cc7c8
Ta Cuong <cuongvt@appota.com>
2024-03-06 11:01:08 +0700

    Update Nginx

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,39 +1,39 @@
 import axios from "axios";
 
 const callApi = async ({ url, data, query, method }) => {
-    const token = localStorage.getItem("token");
-    const baseURL = process.env.REACT_APP_BASE_API_URL;
-    const options = { url, method, baseURL, timeout: 30 * 1000 };
+  const token = localStorage.getItem("token");
+  const baseURL = `${process.env.REACT_APP_BASE_API_URL}/api`;
+  const options = { url, method, baseURL, timeout: 30 * 1000 };
 
-    if (data) {
-        options.data = data;
-    }
-    if (query) {
-        options.params = query;
-    }
-    if (token) {
-        options.headers = { Authorization: token };
-    }
+  if (data) {
+    options.data = data;
+  }
+  if (query) {
+    options.params = query;
+  }
+  if (token) {
+    options.headers = { Authorization: token };
+  }
 
-    const res = await axios(options).catch((err) => {
-        if (err.response) {
-            if (err.response.status === 401) {
-                localStorage.removeItem("token");
-            }
-            if (err.response.status === 500) {
-                throw new Error("Internal server error");
-            }
-            throw new Error(err.response.data.message);
-        } else {
-            if (err.request) {
-                throw new Error("Can't send request to server");
-            } else {
-                throw new Error(err.message);
-            }
-        }
-    });
+  const res = await axios(options).catch((err) => {
+    if (err.response) {
+      if (err.response.status === 401) {
+        localStorage.removeItem("token");
+      }
+      if (err.response.status === 500) {
+        throw new Error("Internal server error");
+      }
+      throw new Error(err.response.data.message);
+    } else {
+      if (err.request) {
+        throw new Error("Can't send request to server");
+      } else {
+        throw new Error(err.message);
+      }
+    }
+  });
 
-    return res.data;
+  return res.data;
 };
 
 export default callApi;
